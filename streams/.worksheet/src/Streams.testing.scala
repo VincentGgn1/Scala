62c0082f2671e43b71d91de47c8a0535209a64d0
Vincent Gueguen <vincentgueguen@mbp-de-vincent.home>
2018-04-29 11:31:32 +0200

    Adding streams project

---
+++ streams/.worksheet/src/Streams.testing.scala
@@ -0,0 +1,10 @@
+package streams
+
+
+
+object testing {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(79); 
+  println("Welcome to the Scala worksheet")}
+  
+ 
+
+}
---
+++ streams/src/test/testing.sc
@@ -0,0 +1,10 @@
+package streams
+
+
+
+object testing {
+  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
+  
+ 
+
+}
